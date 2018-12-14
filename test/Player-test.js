219ffe108b41ee94de6909881c6ff8ed2f267b0f
David Cisneros <Dcisneros101@gmail.com>
2018-12-13 21:02:23 -0700

    No longer able to cheat

--- test/Player-test.js
+++ test/Player-test.js
@@ -3,15 +3,18 @@ const expect = chai.expect;
 const spies = require('chai-spies');
 chai.use(spies);
 const Player = require('../lib/Player.js');
+global.Game = require('../lib/Game.js');
 global.data = require('../lib/data.js');
 global.domUpdates = require('../lib/domUpdates.js');
 
 describe('Player', function() {
 
   let player;
+  let game;
 
   beforeEach(function() {
     player = new Player();
+    game = new Game();
 
     chai.spy.on(global.domUpdates, ['displayNewGame',
      'displayNextRound','spin','displaySpinElement',
@@ -22,5 +25,38 @@ describe('Player', function() {
 
   afterEach(function() {
     chai.spy.restore(global.domUpdates)
+  });
+
+  it('should change player to current player', function() {
+
+    let currentPlayer = game.currentPlayer;
+    player.changePlayerTurn(currentPlayer);
+
+    expect(player.turn).to.equal(true);
+  });
+
+  it('should Reset players ccurrent scores to 0 and add total to total score', function() {
+
+    let currentPlayer = game.currentPlayer;
+    player.addTotalScore(currentPlayer);
+
+    expect(player.score).to.equal(0);
+    expect(player.totalScore).not.to.equal(0);
+  });
+
+  it('should check if currentplayer can buy a vowel', function() {
+
+    let currentPlayer = game.currentPlayer;
+    player.buyVowel();
+
+    expect(player.score).to.toContain(100);
+  });
+
+  it('should remove 100 from score if player buys a vowel', function() {
+
+    let currentPlayer = game.currentPlayer;
+    player.checkVowel();
+
+    expect(player.score).to.equal(true);
   });
 });
